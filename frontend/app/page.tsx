4672dbc6972d0d92249b5dc11ac4fdd0f1480c5c
hatoya65 <hatoya65@gmail.com>
2026-05-25 00:34:33 +0900

    frontend/app/page.tsx更新 frontend/postcss.config.mjs更新

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,45 +3,77 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [summary, setSummary] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
-  const getSummary = async () => {
-    const res = await fetch("http://127.0.0.1:8000/summary");
+  // ファイル選択
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const selectedFile = e.target.files?.[0];
+
+    if (selectedFile) {
+      setFileName(selectedFile.name);
+      setFile(selectedFile);
+    }
+  };
+
+  // アップロード
+  const handleUpload = async () => {
+    if (!file) {
+      alert("ファイルを選択してください");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch(
+      "http://127.0.0.1:8000/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
     const data = await res.json();
 
-    setSummary(data.summary);
+    alert("アップロード成功: " + data.filename);
   };
 
   return (
-    <main
-      style={{
-        padding: "40px",
-        fontFamily: "sans-serif",
-      }}
-    >
-      <h1>会議要約AI</h1>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+      <div className="w-full max-w-4xl bg-white rounded-3xl shadow-2xl p-8">
 
-      <button
-        onClick={getSummary}
-        style={{
-          padding: "10px 20px",
-          fontSize: "18px",
-          cursor: "pointer",
-        }}
-      >
-        要約取得
-      </button>
+        <h1 className="text-4xl font-bold text-blue-700 mb-4">
+          会議要約AI
+        </h1>
 
-      <div
-        style={{
-          marginTop: "30px",
-          padding: "20px",
-          border: "1px solid #ccc",
-          borderRadius: "10px",
-        }}
-      >
-        {summary}
+        <p className="text-gray-600 mb-8">
+          音声ファイルをアップロードしてください
+        </p>
+
+        <div className="border-2 border-dashed border-blue-300 rounded-2xl p-10 bg-blue-50 text-center">
+
+          <input
+            type="file"
+            accept=".mp3,.wav,.m4a"
+            onChange={handleFileChange}
+            className="mb-4"
+          />
+
+          <p className="text-gray-700">
+            {fileName || "ファイル未選択"}
+          </p>
+        </div>
+
+        <button
+          onClick={handleUpload}
+          className="w-full mt-6 bg-blue-600 hover:bg-blue-700 text-white py-4 rounded-2xl text-lg font-bold"
+        >
+          解析開始
+        </button>
       </div>
-    </main>
+    </div>
   );
 }
